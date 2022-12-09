57fb1d6a609208126b594c4cb09d80cdf8ebc1f0
ArtrenH <79058115+ArtrenH@users.noreply.github.com>
2022-12-09 23:45:35 +0100

    Refactored methods: now object oriented, unused features not yet implemented

--- methods.py
+++ methods.py
@@ -2,73 +2,8 @@ import json
 from datetime import datetime
 import requests
 from bs4 import BeautifulSoup
-import os
-
-from models import Plan, Group
-
-
-def get_plan(school_num, date):
-    with open('creds.json') as f:
-        credentials = json.load(f)
-    print(school_num)
-    if school_num not in credentials:
-        return {"error": "School not found"}
-    year, month, day = int(date[:4]), int(date[4:6]), int(date[6:])
-    d = datetime(year, month, day)
-    if d.weekday() > 4:
-        return {"error": "day is on the weekend"}
-    plan_url = f"https://z1.stundenplan24.de/schulen/{credentials[school_num]['school_number']}/mobil/mobdaten/PlanKl{date}.xml"
-    header_stripped = {"authorization": credentials[school_num]["authorization"],}
-    r = requests.get(plan_url, headers=header_stripped)
-    if r.status_code == 404:
-        print("error")
-        return {"error": "plan not available"}
-    #with open("data/test.xml", "w+") as f:
-    #    f.write(r.text)
-    return r
-
-
-def get_latest_plans(school_num):
-    with open('creds.json') as f:
-        credentials = json.load(f)
-    if school_num not in credentials:
-        return {"error": "School not found"}
-    plan_url = f"https://z1.stundenplan24.de/schulen/{credentials[school_num]['school_number']}/mobil/mobdaten/vpinfok.txt"
-    header_stripped = {"authorization": credentials[school_num]["authorization"],}
-    r = requests.get(plan_url, headers=header_stripped)
-    return r
-
-
-# extracts all rooms used in a certain hour from a BeautifulCoup object
-def find_all_rooms_hour(day_data, lesson_num: str):
-    rooms = []
-    lessons = day_data.find_all("std")
-    lessons = [lesson for lesson in lessons if lesson.find('st').text.strip() == lesson_num]
-    for lesson in lessons:
-        cur_room = lesson.find("ra").text.strip()
-        info = lesson.find("if").text.strip()
-        info_bad_keys = ["selbst. (v), Aufgaben wurden erteilt, bitte zu Hause erledigen"]
-        if cur_room != '' and cur_room not in rooms and info not in info_bad_keys:
-            rooms.append(cur_room)
-    return [room for room in rooms if room != ""]
-
-# finds rooms that are not being used in a lesson at a school based on a day
-def find_free_rooms(school_num, day, lesson):
-    if not school_num in os.listdir("data/schools"):
-        return {"error": "requested school not in database"}
-    c = get_plan(school_num=school_num, date=day)
-    if "error" in c:
-        return {"error": "API-Error"}
-    if not "rooms" in os.listdir(f"data/schools/{school_num}"):
-        return {"error": "missing room data"}
-    if not f"room_list_{school_num}.json" in os.listdir(f"data/schools/{school_num}/rooms"):
-        return {"error": "missing room data"}
-    with open(f"data/schools/{school_num}/rooms/room_list_{school_num}.json", "r") as f:
-        all_rooms = json.load(f)["rooms"]
-    day_data = BeautifulSoup(c.text, features="xml")
-    rooms_hour = find_all_rooms_hour(day_data=day_data, lesson_num=lesson)
-    free_rooms = [room for room in all_rooms if room not in rooms_hour]
-    return free_rooms
+from models import Plan
+from pprint import pprint
 
 """
 needs something like this:
@@ -85,56 +20,18 @@ needs something like this:
 </Std>
 """
 def extract_data(std_soup):
-    cur_extract_data = {
-        new_name: std_soup.find(code).text.strip() if std_soup.find(code) else "" for new_name, code in zip(["course_id", "lesson", "begin", "end", "subject", "subject_name", "teacher", "room", "info"], ["nr", "st", "beginn", "ende", "fa", "ku2", "le", "ra", "if"])
-    }
-    for name, key in zip(["subject", "teacher", "room"], ["Fa", "Le", "Ra"]):
-        cur_part = std_soup.find(key.lower())
-        cur_extract_data[f"{name}_changed"] = False
-        if cur_part.get(f"{key.lower()}ae") == f"{key}Geaendert":
-            cur_extract_data[f"{name}_changed"] = True
-    return cur_extract_data
+        cur_extract_data = {
+            new_name: std_soup.find(code).text.strip() if std_soup.find(code) else "" for new_name, code in zip(["course_id", "lesson", "begin", "end", "subject", "subject_name", "teacher", "room", "info"], ["nr", "st", "beginn", "ende", "fa", "ku2", "le", "ra", "if"])
+        }
+        for name, key in zip(["subject", "teacher", "room"], ["Fa", "Le", "Ra"]):
+            cur_part = std_soup.find(key.lower())
+            cur_extract_data[f"{name}_changed"] = False
+            if cur_part.get(f"{key.lower()}ae") == f"{key}Geaendert":
+                cur_extract_data[f"{name}_changed"] = True
+        return cur_extract_data
 
 def find_zusatzinfo(soup):
     return [elem.text.strip() for elem in soup.find("zusatzinfo").find_all("zizeile")] if soup.find("zusatzinfo") else []
-
-def room_lessons(school_num, day, room):
-    c = get_plan(school_num=school_num, date=day)
-    if "error" in c:
-        return {"error": "error with getting the plan for the requested day and school"}
-    day_data = BeautifulSoup(c.text, features="html.parser")
-    room_lessons = []
-    class_plans = day_data.find_all("kl")
-    for class_plan in class_plans:
-        class_name = class_plan.find("kurz").text.strip()
-        class_lessons = class_plan.find_all("std")
-        for class_lesson in class_lessons:
-            room_num = class_lesson.find("ra").text.strip()
-            info = class_lesson.find("if").text.strip()
-            if room_num == room or room in info:
-                room_lessons.append({**{"class": class_name}, **extract_data(class_lesson), "time_data": find_times(day_data, class_name)})
-    zusatzinfo = find_zusatzinfo(day_data)
-    return Plan(school_num, day, room_lessons, zusatzinfo).render()
-
-# extracts all lessons a teacher gives at a certain day
-def teacher_lessons(school_num, day, tag):
-    c = get_plan(school_num=school_num, date=day)
-    if "error" in c:
-        return {"error": "error with getting the plan for the requested day and school"}
-    day_data = BeautifulSoup(c.text, features="html.parser")
-    teacher_lessons = []
-    class_plans = day_data.find_all("kl")
-    for class_plan in class_plans:
-        class_name = class_plan.find("kurz").text.strip()
-        class_lessons = class_plan.find_all("std")
-        for class_lesson in class_lessons:
-            teacher = class_lesson.find("le").text.strip()
-            info = class_lesson.find("if").text.strip()
-            if teacher == tag or tag in info:
-                teacher_lessons.append({**{"class": class_name}, **extract_data(class_lesson), "time_data": find_times(day_data, class_name)})
-    zusatzinfo = find_zusatzinfo(day_data)
-    return Plan(school_num, day, teacher_lessons, zusatzinfo).render()
-
 
 def find_times(plan, course):
     course_data = find_course(plan, course)
@@ -148,75 +45,78 @@ def find_course(plan, course):
             return kl
     return {"error": "chosen class has not been found"}
 
-# gets a plan for a whole class
-def get_plan_normal(school_num, day, course):
-    c = get_plan(school_num=school_num, date=day)
-    if "error" in c:
-        return {"error": "error with getting the plan for the requested day and school"}
-    day_data = BeautifulSoup(c.text, features="html.parser")
-    course_data = find_course(day_data, course)
-    time_data = find_times(day_data, course)
-    pl = course_data.find("pl")
-    std_all = pl.find_all("std")
-    lessons = []
-    for std in std_all:
-        lessons.append({**{"class": course}, **extract_data(std), "time_data": time_data})
-    zusatzinfo = find_zusatzinfo(day_data)
-    return Plan(school_num, day, lessons, zusatzinfo).render()
 
-# gets a plan for a class with only certain courses (for example: course="JG11", courses=["PH1", "MA4"])
-def get_plan_filtered_courses(school_num, day, course, courses):
-    normal_plan = get_plan_normal(school_num, day, course)
-    if "error" in normal_plan:
-        return 
-    return Plan(school_num, day, [lesson for lesson in normal_plan if lesson["subject_name"] in courses or lesson["subject"] in courses]).render()
+class Plan_Extractor():
+    def __init__(self, school_num, date):
+        self.school_num = school_num
+        self.date = date
+        year, month, day = int(date[:4]), int(date[4:6]), int(date[6:])
+        d = datetime(year, month, day)
+        if d.weekday() > 4: raise ValueError("day is on the weekend")
+        with open('creds.json') as f:
+            self.credentials = json.load(f).get(school_num, None)
+        if not self.credentials: raise ValueError("school number not found")
+        print("we got here...")
+        self.get()
+        self.zusatzinfo = find_zusatzinfo(self.day_data)
+    
+    def get(self):
+        plan_url = f"https://z1.stundenplan24.de/schulen/{self.credentials['school_number']}/mobil/mobdaten/PlanKl{self.date}.xml"
+        header_stripped = {"authorization": self.credentials["authorization"],}
+        self.r = requests.get(plan_url, headers=header_stripped)
+        if self.r.status_code == 404:
+            print("error")
+            return {"error": "plan not available"}
+        with open("data/test2.xml", "w+") as f:
+            f.write(self.r.text)
+        self.day_data = BeautifulSoup(self.r.text, "html.parser")
+        return self.r
+    
+    def render(self, lessons):
+        return Plan(self.school_num, self.date, lessons, self.zusatzinfo).render()
+    
+    def teacher_lessons(self, tag):
+        teacher_lessons = []
+        class_plans = self.day_data.find_all("kl")
+        for class_plan in class_plans:
+            class_name = class_plan.find("kurz").text.strip()
+            class_lessons = class_plan.find_all("std")
+            for class_lesson in class_lessons:
+                teacher = class_lesson.find("le").text.strip()
+                info = class_lesson.find("if").text.strip()
+                if teacher == tag or tag in info:
+                    teacher_lessons.append({**{"class": class_name}, **extract_data(class_lesson), "time_data": find_times(self.day_data, class_name)})
+        return self.render(teacher_lessons)
+    
+    def room_lessons(self, room):
+        room_lessons = []
+        class_plans = self.day_data.find_all("kl")
+        for class_plan in class_plans:
+            class_name = class_plan.find("kurz").text.strip()
+            class_lessons = class_plan.find_all("std")
+            for class_lesson in class_lessons:
+                room_num = class_lesson.find("ra").text.strip()
+                info = class_lesson.find("if").text.strip()
+                if room_num == room or room in info:
+                    room_lessons.append({**{"class": class_name}, **extract_data(class_lesson), "time_data": find_times(self.day_data, class_name)})
+        return self.render(room_lessons)
 
-
-def get_classes(school_num):
-    with open('creds.json') as f:
-        credentials = json.load(f)
-    if school_num not in credentials:
-        return {"error": "School not found"}
-    plan_url = f"https://z1.stundenplan24.de/schulen/{credentials[school_num]['school_number']}/mobil/mobdaten/Klassen.xml"
-    header_stripped = {"authorization": credentials[school_num]["authorization"],}
-    r = requests.get(plan_url, headers=header_stripped)
-    soup = BeautifulSoup(r.content.decode("utf-8"), features="xml")
-    classes = soup.find_all("Kl")
-    class_data = {"kurse": {}, "unterricht": {}}
-    for cur_class in classes:
-        class_data["kurse"][cur_class.find("Kurz").text] = [
-            {"course": ku.find("KKz").text,
-            "teacher": ku.find("KKz").get("KLe", "")} for ku in cur_class.find("Kurse").find_all("Ku")
-        ]
-        class_data["unterricht"][cur_class.find("Kurz").text] = {ku.find("UeNr").text: {
-        #c = {ku.find("UeNr").text: {
-            "id": ku.find("UeNr").text,
-            "teacher": ku.find("UeNr").get("UeLe", ""),
-            "subject": ku.find("UeNr").get("UeFa", ""),
-            "course": ku.find("UeNr").get("UeGr", "")
-        } for ku in cur_class.find("Unterricht").find_all("Ue")}
-    with open(f"data/{school_num}.json", "w+") as f:
-        json.dump(class_data, f, indent=2, ensure_ascii=False)
-
-    return r
-
-
-def load_courses(school_num, group):
-    with open(f"data/{school_num}.json", "r") as f:
-        data = json.load(f)
-    if group not in data["unterricht"]:
-        return {"error": "Klasse nicht gefunden"}
-    all_courses = data["unterricht"][group].values()
-    return Group(school_num, all_courses).render()
-
+    def get_plan_normal(self, course):
+        course_data = find_course(self.day_data, course)
+        time_data = find_times(self.day_data, course)
+        pl = course_data.find("pl")
+        std_all = pl.find_all("std")
+        lessons = []
+        for std in std_all:
+            lessons.append({**{"class": course}, **extract_data(std), "time_data": time_data})
+        return self.render(lessons)
+    
+    def get_plan_filtered_courses(self, course, courses):
+        normal_plan= self.get_plan_normal(course)
+        return self.render([lesson for lesson in normal_plan[0] if lesson["subject_name"] in courses or lesson["subject"] in courses])
+    
 
 
 if __name__ == "__main__":
-    pass
-    #get_plan("10001329", "20221209")
-    #print(load_courses("10001329", "JG12"))
-    get_plan_normal("10001329", "20221209", "JG12")
-    #print(get_plan_normal("10001329", "20221207", "JG12"))
-    #print(get_plan_filtered_courses("10001329", "20221205", "JG12", ["MA1", "MA2", "MA3", "MA4"]))
-    #print(teacher_lessons("10001329", "20221207", "GLS"))
-    #print(room_lessons("10001329", "20221205", "2312"))
+    p = Plan_Extractor("10001329", "20221209").teacher_lessons("SCH")
+    pprint(p)
--- server.py
+++ server.py
@@ -1,7 +1,7 @@
 import json
 from flask import Flask, redirect, render_template
 import datetime
-import methods
+from methods import Plan_Extractor
 from vplan_utils import add_spacers, remove_duplicates, convert_date_readable
 
 app = Flask(__name__)
@@ -28,36 +28,36 @@ def schulnummer(schulnummer):
 
 @app.route('/<schulnummer>/<date>')
 def schulplan(schulnummer, date):
-    return methods.get_plan(school_num=schulnummer, date=date).content
+    return Plan_Extractor(schulnummer, date).r.content
 
 @app.route('/<schulnummer>/<date>/lehrerplan/<kuerzel>')
 def lehrerplan(schulnummer, date, kuerzel):
-    data, zusatzinfo = methods.teacher_lessons(schulnummer, date, kuerzel)
+    data, zusatzinfo = Plan_Extractor(schulnummer, date).teacher_lessons(kuerzel)
     return render_template('plan.html', plan_type="Lehrer", plan_value=kuerzel, date=convert_date_readable(date), plan=add_spacers(remove_duplicates(data)), zusatzinfo=zusatzinfo)
 
 @app.route('/<schulnummer>/<date>/raumplan/<room_num>')
 def raumplan(schulnummer, date, room_num):
-    data, zusatzinfo = methods.room_lessons(schulnummer, date, room_num)
+    data, zusatzinfo = Plan_Extractor(schulnummer, date).room_lessons(room_num)
     return render_template('plan.html', plan_type="Raum", plan_value=room_num, date=convert_date_readable(date), plan=add_spacers(remove_duplicates(data)), zusatzinfo=zusatzinfo)
 
 @app.route('/<schulnummer>/<date>/klassenplan/<klasse>')
 def klassenplan(schulnummer, date, klasse):
     klasse = klasse.replace("_", "/")
-    data, zusatzinfo = methods.get_plan_normal(schulnummer, date, klasse)
+    data, zusatzinfo = Plan_Extractor(schulnummer, date).get_plan_normal(klasse)
     return render_template('plan.html', plan_type="Klasse", plan_value=klasse, date=convert_date_readable(date), plan=add_spacers(remove_duplicates(data)), zusatzinfo=zusatzinfo)
 
 @app.route('/<schulnummer>/<date>/plan/<klasse>/<kurse>')
 def plan(schulnummer, date, klasse, kurse):
     kurse = kurse.split(",")
-    data, zusatzinfo = methods.get_plan_filtered_courses(schulnummer, date, klasse, kurse)
+    data, zusatzinfo = Plan_Extractor(schulnummer, date).get_plan_filtered_courses(klasse, kurse)
     return render_template('plan.html', plan_type="Klasse", plan_value=klasse, date=convert_date_readable(date), plan=add_spacers(remove_duplicates(data)), zusatzinfo=zusatzinfo)
 
 
-@app.route('/<schulnummer>/<date>/plan/<klasse>')
+"""@app.route('/<schulnummer>/<date>/plan/<klasse>')
 def courses(schulnummer, date, klasse):
     klasse = klasse.replace("_", "/")
     data = methods.load_courses(schulnummer, klasse)
-    return render_template('courses.html', plan_type="Klasse", plan_value=klasse, date=convert_date_readable(date), courses=add_spacers(remove_duplicates(data)))
+    return render_template('courses.html', plan_type="Klasse", plan_value=klasse, date=convert_date_readable(date), courses=add_spacers(remove_duplicates(data)))"""
 
 #@app.route("/<schulnummer>/<group>")
 #def courses(schulnummer, group):
