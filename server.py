323e66dc822a7c2ebf1b8c1bf0354b89da5fb425
OfficialFreak <npfreak@t-online.de>
2022-12-10 09:21:02 +0100

    PWA + Mobile Styling + Scraper-Blocking

--- server.py
+++ server.py
@@ -1,5 +1,5 @@
 import json
-from flask import Flask, redirect, render_template
+from flask import Flask, redirect, render_template, make_response
 import datetime
 from methods import Plan_Extractor
 from vplan_utils import add_spacers, remove_duplicates, convert_date_readable
@@ -52,6 +52,17 @@ def plan(schulnummer, date, klasse, kurse):
     data, zusatzinfo = Plan_Extractor(schulnummer, date).get_plan_filtered_courses(klasse, kurse)
     return render_template('plan.html', plan_type="Klasse", plan_value=klasse, date=convert_date_readable(date), plan=add_spacers(remove_duplicates(data)), zusatzinfo=zusatzinfo)
 
+@app.route('/sw.js')
+def service_worker():
+    response = make_response("""self.addEventListener ("fetch", function(event) {});""", 200)
+    response.mimetype = "application/javascript"
+    return response
+
+@app.route('/robots.txt')
+def robots():
+    response = make_response("""User-agent: *\nDisallow: /""", 200)
+    response.mimetype = "text/plain"
+    return response
 
 """@app.route('/<schulnummer>/<date>/plan/<klasse>')
 def courses(schulnummer, date, klasse):
@@ -68,3 +79,4 @@ print("http://127.0.0.1:5010/10001329/20221209/klassenplan/JG12")
 
 
 app.run(port=5010)
+#app.run(host='0.0.0.0', port=5010)
