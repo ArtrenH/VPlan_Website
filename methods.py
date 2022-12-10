3f58e477b88152f3bae15879cbf6126003e90900
ArtrenH <79058115+ArtrenH@users.noreply.github.com>
2022-12-10 14:24:17 +0100

    changed bs4 parser from html.parser to xml

--- methods.py
+++ methods.py
@@ -21,27 +21,27 @@ needs something like this:
 """
 def extract_data(std_soup):
         cur_extract_data = {
-            new_name: std_soup.find(code).text.strip() if std_soup.find(code) else "" for new_name, code in zip(["course_id", "lesson", "begin", "end", "subject", "subject_name", "teacher", "room", "info"], ["nr", "st", "beginn", "ende", "fa", "ku2", "le", "ra", "if"])
+            new_name: std_soup.find(code).text.strip() if std_soup.find(code) else "" for new_name, code in zip(["course_id", "lesson", "begin", "end", "subject", "subject_name", "teacher", "room", "info"], ["Nr", "St", "Beginn", "Ende", "Fa", "Ku2", "Le", "Ra", "If"])
         }
         for name, key in zip(["subject", "teacher", "room"], ["Fa", "Le", "Ra"]):
-            cur_part = std_soup.find(key.lower())
+            cur_part = std_soup.find(key)
             cur_extract_data[f"{name}_changed"] = False
-            if cur_part.get(f"{key.lower()}ae") == f"{key}Geaendert":
+            if cur_part.get(f"{key}Ae") == f"{key}Geaendert":
                 cur_extract_data[f"{name}_changed"] = True
         return cur_extract_data
 
 def find_zusatzinfo(soup):
-    return [elem.text.strip() for elem in soup.find("zusatzinfo").find_all("zizeile")] if soup.find("zusatzinfo") else []
+    return [elem.text.strip() for elem in soup.find("ZusatzInfo").find_all("ZiZeile")] if soup.find("ZusatzInfo") else []
 
 def find_times(plan, course):
     course_data = find_course(plan, course)
-    time_data = course_data.find("klstunden")
-    time_data = {tag.text: {"lesson": tag.text, "begin": tag.get("zeitvon", "?"), "end": tag.get("zeitbis", "?")} for tag in time_data.find_all("klst")}
+    time_data = course_data.find("KlStunden")
+    time_data = {tag.text: {"lesson": tag.text, "begin": tag.get("ZeitVon", "?"), "end": tag.get("ZeitBis", "?")} for tag in time_data.find_all("KlSt")}
     return time_data
 
 def find_course(plan, course):
-    for kl in plan.find_all("kl"):
-        if kl.find("kurz").text.strip() == course:
+    for kl in plan.find_all("Kl"):
+        if kl.find("Kurz").text.strip() == course:
             return kl
     return {"error": "chosen class has not been found"}
 
@@ -69,7 +69,8 @@ class Plan_Extractor():
             return {"error": "plan not available"}
         with open("data/test2.xml", "w+") as f:
             f.write(self.r.text)
-        self.day_data = BeautifulSoup(self.r.text, "html.parser")
+        #self.day_data = BeautifulSoup(self.r.text, "html.parser")
+        self.day_data = BeautifulSoup(self.r.text, "xml")
         return self.r
     
     def render(self, lessons):
@@ -77,26 +78,27 @@ class Plan_Extractor():
     
     def teacher_lessons(self, tag):
         teacher_lessons = []
-        class_plans = self.day_data.find_all("kl")
+        class_plans = self.day_data.find_all("Kl")
+        print(len(class_plans))
         for class_plan in class_plans:
-            class_name = class_plan.find("kurz").text.strip()
-            class_lessons = class_plan.find_all("std")
+            class_name = class_plan.find("Kurz").text.strip()
+            class_lessons = class_plan.find_all("Std")
             for class_lesson in class_lessons:
-                teacher = class_lesson.find("le").text.strip()
-                info = class_lesson.find("if").text.strip()
+                teacher = class_lesson.find("Le").text.strip()
+                info = class_lesson.find("If").text.strip()
                 if teacher == tag or tag in info:
                     teacher_lessons.append({**{"class": class_name}, **extract_data(class_lesson), "time_data": find_times(self.day_data, class_name)})
         return self.render(teacher_lessons)
     
     def room_lessons(self, room):
         room_lessons = []
-        class_plans = self.day_data.find_all("kl")
+        class_plans = self.day_data.find_all("Kl")
         for class_plan in class_plans:
-            class_name = class_plan.find("kurz").text.strip()
-            class_lessons = class_plan.find_all("std")
+            class_name = class_plan.find("Kurz").text.strip()
+            class_lessons = class_plan.find_all("Std")
             for class_lesson in class_lessons:
-                room_num = class_lesson.find("ra").text.strip()
-                info = class_lesson.find("if").text.strip()
+                room_num = class_lesson.find("Ra").text.strip()
+                info = class_lesson.find("If").text.strip()
                 if room_num == room or room in info:
                     room_lessons.append({**{"class": class_name}, **extract_data(class_lesson), "time_data": find_times(self.day_data, class_name)})
         return self.render(room_lessons)
@@ -104,8 +106,8 @@ class Plan_Extractor():
     def get_plan_normal(self, course):
         course_data = find_course(self.day_data, course)
         time_data = find_times(self.day_data, course)
-        pl = course_data.find("pl")
-        std_all = pl.find_all("std")
+        pl = course_data.find("Pl")
+        std_all = pl.find_all("Std")
         lessons = []
         for std in std_all:
             lessons.append({**{"class": course}, **extract_data(std), "time_data": time_data})
@@ -118,5 +120,5 @@ class Plan_Extractor():
 
 
 if __name__ == "__main__":
-    p = Plan_Extractor("10001329", "20221209").teacher_lessons("SCH")
+    p = Plan_Extractor("10001329", "20221209").get_plan_filtered_courses("JG12", ["de2"])
     pprint(p)
