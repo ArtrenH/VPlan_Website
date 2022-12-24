66c353a1e6ef67fff0ca8a6866d8cb6349aeb189
OfficialFreak <npfreak@t-online.de>
2022-12-24 14:05:31 +0100

    downloaded a few libraries + loading of templates

---
+++ static/js/base.js
@@ -0,0 +1,11 @@
+navigator.serviceWorker && navigator.serviceWorker.register("/sw.js").then(function(registration) {});
+
+$.ajax({
+    type: 'GET',
+    url: '/10001329?date=20221221&type=klasse&klasse=JG12',
+    dataType: 'html',
+    success: function(response) {
+        $('.vplan-wrapper').remove();
+        $('.loaded_content').append(response);
+    }
+});
