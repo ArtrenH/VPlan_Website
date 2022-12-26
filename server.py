d05e497bf67542914f546fd714edbaf95995b0b2
OfficialFreak <npfreak@t-online.de>
2022-12-26 17:34:33 +0100

    Schulauswahl designed

--- server.py
+++ server.py
@@ -47,7 +47,9 @@ def request_loader(request):
 @app.route('/')
 @login_required
 def index():
-    return render_template('start.html')
+    with open("creds.json", "r") as f:
+        tmp_data = json.load(f)
+        return render_template('start.html', available_schools=[item[1]["school_name"] for item in tmp_data.items()])
     return redirect(url_for('handle_plan', schulnummer="10001329"))
 
 @login_manager.unauthorized_handler
