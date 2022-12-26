770fd197a5a2f53a3629a5a853c970c5fb0c7aef
OfficialFreak <npfreak@t-online.de>
2022-12-26 19:39:45 +0100

    datepicker

--- static/js/index.js
+++ static/js/index.js
@@ -10,10 +10,22 @@ if (vorangezeigt) {
     });
 }
 document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.modal');
-    var instances = M.Modal.init(elems, {
+    var modal_elems = document.querySelectorAll('.modal');
+    var modal_instances = M.Modal.init(modal_elems, {
         onCloseStart: function(elem) {
             elem.scrollTo(0, 0);
         }
     });
+    var nextDate = new Date(Date.parse(`${default_date.substring(0, 4)}-${default_date.substring(4, 6)}-${default_date.substring(6, 8)}`));
+    var datepicker_elems = document.querySelectorAll('.datepicker');
+    var datepicker_instances = M.Datepicker.init(datepicker_elems, {
+        autoClose: true,
+        defaultDate: nextDate,
+        setDefaultDate: true,
+        onSelect: function(date) {
+            selected_date = `${date.getFullYear()}${date.getMonth()+1}${date.getDate()}`;
+            document.getElementById('selected_time').innerHTML = `${date.getDate()}.${date.getMonth()+1}.${date.getFullYear()}`;
+        }
+    },
+    firstDay: 1);
 });
