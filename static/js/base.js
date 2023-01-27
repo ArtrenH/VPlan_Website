594bee536a3caab922cd00ab05f6cd67ffe64caf
OfficialBrot <36410565+OfficialFreak@users.noreply.github.com>
2023-01-28 00:18:19 +0100

    Preferences Design Mockup

--- static/js/base.js
+++ static/js/base.js
@@ -1,11 +1,21 @@
 navigator.serviceWorker && navigator.serviceWorker.register("/sw.js").then(function(registration) {});
-
-/*$.ajax({
-    type: 'GET',
-    url: '/10001329?date=20221221&type=klasse&klasse=JG12',
-    dataType: 'html',
-    success: function(response) {
-        $('.vplan-wrapper').remove();
-        $('.loaded_content').append(response);
-    }
-});*/
+document.addEventListener('DOMContentLoaded', function() {
+    var dropdown_elems = document.querySelectorAll('.dropdown-trigger');
+    var dropdown_instances = M.Dropdown.init(dropdown_elems, {
+        coverTrigger: false,
+        alignment: 'right',
+        constrainWidth: false
+    });
+    var modal_elems = document.querySelectorAll('.preference_modal');
+    var modal_instances = M.Modal.init(modal_elems, {
+        onOpenStart: function() {
+            // Load in all Classes
+            alert(1);
+        },
+        onCloseStart: function(elem) {
+            elem.scrollTo(0, 0);
+            // Save preferences
+            alert(2);
+        }
+    });
+});
